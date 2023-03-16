0c19e1668a8b6b33334ba964c5c4f83f01ea384b
PC03 <PC03@INFO5-5374>
2023-03-16 08:04:50 +0100

    Aggiungere i file di progetto.

---
+++ AS2223_4G_INF_PerilliCristian_API/Controllers/CompitoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AS2223_4G_INF_PerilliCristian_API.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/v1/")]
+    public class SusController : Controller
+    {
+        [HttpGet("division")]
+        public JsonResult GetDivision(double numerator, double denominator)
+        {
+            if (denominator == 0)
+            {
+                return Json(new { status = "error" });
+            }
+
+            return Json(new
+            {
+                result = numerator / denominator,
+                status = 200
+            });
+        }
+    }
+}
